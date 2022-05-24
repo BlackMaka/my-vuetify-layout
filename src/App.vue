9f712fd267e4adfec19553dc5a78b16540e78509
suhan <completed0728@gmail.com>
2022-05-24 17:37:19 +0900

    test코드

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,27 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <SideDrawerBar @drawer="drawer = !drawer" />
     <v-main>
       <router-view />
     </v-main>
+
+    <BottomNavigation fixed />
   </v-app>
 </template>
 
 <script>
+//import BottomNavigation from "@/views/BottomNavigation";
+import SideDrawerBar from "../src/views/SideBar.vue";
+import BottomNavigation from "../src/views/BottomNavigation.vue";
 export default {
-  name: "App",
-
-  data: () => ({
-    //
-  }),
+  components: {
+    BottomNavigation,
+    SideDrawerBar,
+  },
+  data() {
+    return {
+      drawer: false,
+    };
+  },
 };
 </script>
---
+++ src/views/SideBar.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <v-app-bar-nav-icon @click.stop="$emit('drawer')"></v-app-bar-nav-icon>
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+    </div>
+
+    <v-spacer></v-spacer>
+    <v-btn
+      href="https://github.com/vuetifyjs/vuetify/releases/latest"
+      target="_blank"
+      text
+    >
+      <span class="mr-2">Latest Release</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+
+    <v-navigation-drawer absolute bottom temporary>
+      <v-list nav dense>
+        <v-list-item-group active-class="deep-purple--text text--accent-4">
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "SideDrawerBar",
+};
+</script>
+
+<style></style>
