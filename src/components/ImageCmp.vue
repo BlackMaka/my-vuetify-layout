a9b213696005f79e320ed8f3c87c0b9cba66cd42
suhan <completed0728@gmail.com>
2022-05-27 17:50:48 +0900

    상품상세페이지 구현

--- src/components/ImageCmp.vue
+++ src/components/ImageCmp.vue
@@ -2,13 +2,16 @@
   <v-img
     class="white--text align-end"
     height="200px"
+    v-bind="$attrs"
     src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
   >
   </v-img>
 </template>
 
 <script>
-  export default {};
+  export default {
+    inheritAttrs: false,
+  };
 </script>
 
 <style></style>
